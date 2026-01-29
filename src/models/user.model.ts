ff3dab79c5cd42014fc8bc626cd24143f59888e1
LeeGMin <yumi3545@naver.com>
2026-01-29 09:54:07 +0000

    feat: 초기 데이터베이스 스키마 설계

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,16 +4,24 @@ export class User {
   id: number;
   email: string;
   name: string;
-  employeeId: string | null;
-  contact: string | null;
-  companyId: number;
+  employeeNumber: string;
+  phoneNumber: string;
+  imageUrl: String | null;
+  isAdmin: boolean;
+  company: {
+    companyName: string;
+  };
 
   constructor(data: PrismaUser) {
     this.id = data.id;
     this.email = data.email;
     this.name = data.name;
-    this.employeeId = data.employeeId;
-    this.contact = data.contact;
-    this.companyId = data.companyId;
+    this.employeeNumber = data.employeeNumber;
+    this.phoneNumber = data.phoneNumber;
+    this.imageUrl = data.imageUrl;
+    this.isAdmin = data.isAdmin;
+    this.company = {
+      companyName: data.company.companyName,
+    };
   }
 }
