dc01a5f5ac300c73d76768da5ca21269a92e3c3e
apple <test@testui-MacBookAir.local>
2026-02-02 18:41:39 +0900

    chore: 서버 실행 로직 및 에러 추가

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 //express 5 버전부터는 try/catch를 사용하지 않아도 알아서 에러를 넘겨줌
 import express from 'express';
 import { errorHandler } from './errors/errorHandler';
+import authRouter from './routes/auth.route';
+import userRouter from './routes/user.route';
 
 const app = express();
 
@@ -9,8 +11,16 @@ app.use(express.json()); // JSON 요청 바디 파싱
 app.use(express.urlencoded({ extended: true })); // URL-encoded 파싱
 
 // 라우터 설정 (계획서의 routes 폴더 활용)
+app.use('/auth', authRouter);
+app.use('/users', userRouter);
 
 //에러 핸들러 설정 (반드시 라우터보다 아래에 위치!)
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 서버가 http://localhost:${PORT} 에서 실행 중입니다!`);
+});
 
 export default app;
