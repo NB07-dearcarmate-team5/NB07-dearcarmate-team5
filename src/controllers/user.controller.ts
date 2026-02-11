bc298288979c855c4a7e0651cbf94827d3791700
apple <test@testui-MacBookAir.local>
2026-02-11 11:47:21 +0900

    refactor: 유저, 인증 스웨거 응답바디 추가 및 비즈니스 로직 리팩토링

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -10,19 +10,19 @@ export class UserController {
   getProfile = async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.user!.userId;
     const user = await this.userService.getUserProfile(userId);
-    return res.status(200).json({ success: true, data: user });
+    return res.status(200).json(user);
   };
 
   updateProfile = async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.user!.userId;
     const updatedUser = await this.userService.updateUser(userId, req.body);
-    return res.status(200).json({ success: true, data: updatedUser });
+    return res.status(200).json(updatedUser);
   };
 
   deleteAccount = async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.user!.userId;
     const result = await this.userService.deleteUser(userId);
-    return res.status(200).json({ success: true, ...result });
+    return res.status(200).json(result);
   };
 
   deleteUserByAdmin = async (req: Request, res: Response, next: NextFunction) => {
